b8c2ad1c8fdba8969b1c6aa855d81abdc37df153
eqinox <86393056685015v>
2021-08-27 18:09:01 +0300

    create portal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import classes from "./Modal.module.css";
+import reactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {reactDOM.createPortal(<Backdrop />, portalElement)}
+      {reactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
